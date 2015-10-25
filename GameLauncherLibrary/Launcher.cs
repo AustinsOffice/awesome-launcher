01c3faf1d6359419685ebcbe9c37dbd1d8f5757c
Austin Gregory <awfulmedia@gmail.com>
2015-10-25 00:02:50 -0400

    Initial Commit

---
+++ GameLauncherLibrary/Launcher.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Diagnostics;
+using LitJson;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLauncherLibrary
+{
+    public class Launcher
+    {
+        public static void PlayGame()
+        {
+            Process.Start(Directory.GetCurrentDirectory() + "/Game.exe");
+        }
+
+        public static void LaunchWebsite(string url)
+        {
+            Process.Start(url);
+        }
+    }
+}
---
+++ GameLauncherTest/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+using GameLauncherLibrary;
+namespace GameLauncherTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void websiteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Launcher.LaunchWebsite("http://www.awfulmedia.com/");
+        }
+
+        private void launchButton_Click(object sender, RoutedEventArgs e)
+        {
+            Launcher.PlayGame();
+            Environment.Exit(0);
+        }
+    }
+}
